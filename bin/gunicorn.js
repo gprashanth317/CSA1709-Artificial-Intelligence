867bee43c8290f0ec2494d3bfa6009259edc3ab4
gprashanth317 <gprashanth317@gmail.com>
2026-09-04 23:22:49 +0530

    Auto-link gunicorn command to Node server during build in Node.js environment

--- bin/gunicorn.js
+++ bin/gunicorn.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 // Node.js fallback wrapper for gunicorn command when deployed in Node environment
-require('../server.js');
+const path = require('path');
+const fs = require('fs');
+
+const cwdServer = path.join(process.cwd(), 'server.js');
+const relServer = path.join(__dirname, '..', 'server.js');
+
+if (fs.existsSync(cwdServer)) {
+  require(cwdServer);
+} else if (fs.existsSync(relServer)) {
+  require(relServer);
+} else {
+  console.error('Could not locate server.js at', cwdServer);
+  process.exit(1);
+}
